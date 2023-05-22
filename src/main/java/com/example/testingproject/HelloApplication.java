d5832ab9dfef71afcabc0f4d08e47ad4d24b955f
seif medhat <seifmedhat2001@gmail.com>
2023-05-22 23:52:40 +0300

    login gui

--- src/main/java/com/example/testingproject/HelloApplication.java
+++ src/main/java/com/example/testingproject/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
@@ -19,6 +19,5 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch();
-        System.out.println("sei medhat");
     }
 }
--- src/main/java/com/example/testingproject/HelloController.java
+++ src/main/java/com/example/testingproject/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.testingproject;
-
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loginbtn;
+
+    @FXML
+    private PasswordField passwordtxt;
+
+    @FXML
+    private TextField usernametxt;
+
+    @FXML
+    void loginbtnClick(ActionEvent event) {
+        System.out.println("hi");
     }
+
 }
